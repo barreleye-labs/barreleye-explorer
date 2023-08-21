068e032487e2641d5349395ae0b839841e0a8369
usiyoung <usiyoung7@gmail.com>
2023-08-22 01:42:39 +0900

    feat(constants): defined nav, tableInfo

---
+++ constants/tableInfo.ts
@@ -0,0 +1,69 @@
+export const dataSource = [
+  {
+    key: '1',
+    block: 80936777,
+    time: '3 hours ago',
+    totalTxs: 0,
+    hash: '0x0863...0bafe2',
+    size: '1,026'
+  },
+  {
+    key: '2',
+    block: 809323777,
+    time: '3 hours ago',
+    totalTxs: 0,
+    hash: '0x0863...0bafe2',
+    size: '1,026'
+  },
+  {
+    key: '2',
+    block: 809323777,
+    time: '3 hours ago',
+    totalTxs: 0,
+    hash: '0x0863...0bafe2',
+    size: '1,026'
+  },
+  {
+    key: '2',
+    block: 809323777,
+    time: '3 hours ago',
+    totalTxs: 0,
+    hash: '0x0863...0bafe2',
+    size: '1,026'
+  }
+
+]
+
+export const columns = [
+  {
+    title: 'BLOCK',
+    dataIndex: 'block',
+    key: 'block',
+    align: 'center'
+
+  },
+  {
+    title: 'TIME',
+    dataIndex: 'time',
+    key: 'time',
+    align: 'center'
+  },
+  {
+    title: 'TOTAL TXS',
+    dataIndex: 'totalTxs',
+    key: 'totalTxs',
+    align: 'center'
+  },
+  {
+    title: 'HASH',
+    dataIndex: 'hash',
+    key: 'hash',
+    align: 'center'
+  },
+  {
+    title: 'SIZE(BYTE)',
+    dataIndex: 'size',
+    key: 'size',
+    align: 'center'
+  }
+]
