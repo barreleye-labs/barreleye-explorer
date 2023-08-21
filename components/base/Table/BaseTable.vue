e8ff7e92ffbcab2d3cad5f02171234a3f6936e0f
usiyoung <usiyoung7@gmail.com>
2023-08-22 01:39:11 +0900

    feat(components): development of common table components

---
+++ components/base/Table/BaseTable.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Props } from './types'
+
+const props = defineProps<Props>()
+
+const { dataSource, columns, loading, size } = props
+</script>
+
+<template>
+  <div>
+    <a-table
+      :pagination="false"
+      :data-source="dataSource"
+      :columns="columns"
+      :loading="loading"
+      :size="size"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.ant-table-thead > tr > th){
+  font-size: 11px;
+  font-weight: 400;
+  text-align: center;
+}
+
+:deep(.ant-table-tbody > tr > td){
+  font-size: 11px;
+}
+</style>
---
+++ components/base/Table/RouteTable.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { Props } from './types'
+
+const props = defineProps<Props>()
+
+const { dataSource, columns } = props
+</script>
+
+<template>
+  <div>
+    <a-table :pagination="true" :data-source="dataSource" :columns="columns">
+      <template #title>
+        {{ ($route.name as String).toUpperCase() }}
+      </template>
+    </a-table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.ant-table){
+  padding-bottom: 2rem;
+  border-radius: 9px;
+  height:70vh;
+}
+:deep(.ant-table-thead > tr > th){
+  font-size: 11px;
+  font-weight: 400;
+  text-align: center;
+}
+
+:deep(.ant-table-tbody > tr > td){
+  font-size: 11px;
+}
+</style>
